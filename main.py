18d3e8f80c3c2552e95566f1236c7333a5494477
Quentin O'Neal <qbicle14@gmail.com>
2023-10-16 21:15:22 -0400

    hope this works

---
+++ main.py
@@ -0,0 +1,10 @@
+# @TODO: Write a function that returns the arithmetic average for a list of numbers
+
+def average(number):
+    return sum(number) / len(number) 
+
+# Test your function with the following:
+# print(average([1, 5, 9]))
+# print(average(range(11)))
+
+print(average([1, 2, 3, 4, 5, 6, 7, 8]))
